1ba5ca739c23ae350bfdf5d45a688569b18a68a4
XIAO Xiang <xiaoxiang@hkbu.edu.hk>
2024-12-07 23:04:32 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["r2s.0ii0.eu.org"];
+pub const PUBLIC_RS_PUB_KEY: &str = "P4FD0XZmzBToZAJBOpxJ5Hgn+kgCunDrtcU967q+nKM=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
